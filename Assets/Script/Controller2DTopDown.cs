672ab8bc2f8d35d9524ecea696d6f9113048a8bd
e012052020alice <e012052020alice@gmail.com>
2022-11-27 15:59:55 +0800

    Upade 人物動畫2

---
+++ Assets/Script/Controller2DTopDown.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChimmyBear
+{
+    public class Controller2DTopDown : MonoBehaviour
+    {
+        /// <summary>
+        /// 2D Top Down 類型控制器
+        /// </summary>
+    }
+
+}
+
