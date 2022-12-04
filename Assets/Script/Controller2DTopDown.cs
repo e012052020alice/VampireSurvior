6b8d32d822436f4279f8745faa0aa1dcae6c7b0b
e012052020alice <119144400+e012052020alice@users.noreply.github.com>
2022-12-04 14:39:38 +0800

    Update控制系統

--- Assets/Script/Controller2DTopDown.cs
+++ Assets/Script/Controller2DTopDown.cs
@@ -4,11 +4,45 @@ using UnityEngine;
 
 namespace ChimmyBear
 {
+    /// <summary>
+    /// 2D Top Down 類型控制器
+    /// </summary>
     public class Controller2DTopDown : MonoBehaviour
     {
-        /// <summary>
-        /// 2D Top Down 類型控制器
-        /// </summary>
+        [SerializeField,Header("移動速度"),Range(0,100)]
+        private float speed = 3.5f;
+        private Animator ani;
+        private Rigidbody2D rig;
+        private string parWalk = "走路開關";
+
+        private void Awake()
+        {
+            ani = GetComponent<Animator>();
+            rig = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+        private void Move()
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            //print($"玩家的水平軸向值:{h}");
+            rig.velocity = new Vector2(h, v) * speed;
+            UpdateAnimation(h, v);
+            Flip(h);
+        }
+        private void UpdateAnimation(float h,float v)
+        {
+            ani.SetBool(parWalk, h != 0 || v != 0);
+        }
+        private void Flip(float h)
+        {
+            if (Mathf.Abs(h) < 0.1f) return;
+            transform.eulerAngles = new Vector2(0, h < 0 ? 0 : 180);
+        }
     }
 
 }
